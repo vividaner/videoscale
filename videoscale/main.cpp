a491937b5c5b17c34625a2ff4770d075901aa4cb
Dan <miaodaner@gmail.com>
2015-02-02 14:44:05 +0800

    video scale

---
+++ videoscale/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  videoscale
+//
+//  Created by DanMiao on 1/30/15.
+//  Copyright (c) 2015 DanMiao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
